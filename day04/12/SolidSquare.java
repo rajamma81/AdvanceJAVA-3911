1a2fe6085ec286c957bc749f3c0354296491197d
Rajamma <RajiRadhamma5@gmail.com>
2025-12-05 21:43:13 +0530

    qs

--- day04/12/SolidSquare.java
+++ day04/12/SolidSquare.java
@@ -1,6 +1,6 @@
 public class SolidSquare {
     public static void main(String[] args) {
-        int n = 5;  // square size
+        int n = 5;  
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
