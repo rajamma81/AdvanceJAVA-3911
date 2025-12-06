781e9dc5d3a50af0e89f6574e383066ea2cee4a6
Rajamma <RajiRadhamma5@gmail.com>
2025-12-06 21:48:00 +0530

    qs

---
+++ day1/Student.java
@@ -0,0 +1,15 @@
+
+public class Student {
+
+    public static void main(String[] args){
+
+    String name; //variable declaration
+	name = "Vanisha";//variable initialization
+		
+	int age = 25;//variable declaration and initialization
+		
+	System.out.println("Student name : " + name);
+	System.out.println("Student age : " + age); 
+    
+}
+}
