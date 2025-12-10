8d7d3fe74fe76f999ebe7c61a8a44ecad2e13bb4
Rajamma <RajiRadhamma5@gmail.com>
2025-12-10 10:02:13 +0530

    qs

---
+++ day2/ExampleForDoWhile.java
@@ -0,0 +1,16 @@
+
+public class ExampleForDoWhile {
+    public static void main(String[] args) {
+		
+		int x = 1;
+		
+		do {
+			System.out.println(x);
+			x++;
+		}
+		while(x<=5);
+
+	}
+
+    
+}
